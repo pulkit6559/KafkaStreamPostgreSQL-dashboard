9d533f2efbb06e5edbd2f9c3f26c62697abc4a1f
pulkit6559 <pulkitarora7.fas@gmail.com>
2024-01-18 01:33:06 +0100

    producer

---
+++ producer.py
@@ -0,0 +1,42 @@
+from confluent_kafka import Producer
+import json
+import websocket
+
+class KafkaProducer:
+    def __init__(self, bootstrap_servers):
+        self.producer = Producer({'bootstrap.servers': bootstrap_servers})
+
+    def produce(self, topic, key, value):
+        self.producer.produce(topic, key=key, value=value)
+
+    def flush(self):
+        self.producer.flush()
+
+
+class WebSocketProducer:
+    def __init__(self, websocket_url, subscribe_message, kafka_producer):
+        self.websocket_url = websocket_url
+        self.subscribe_message = subscribe_message
+        self.kafka_producer = kafka_producer
+
+    def on_message(self, ws, message):
+        try:
+            data = json.loads(message)
+            product_id = data.get('product_id')
+
+            if product_id in ["ETH-BTC", "ETH-USD"]:
+                kafka_topic = f"coinbase_{product_id.lower()}_topic"
+                self.kafka_producer.produce(kafka_topic, key=product_id, value=json.dumps(data))
+                self.kafka_producer.flush()
+
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+
+    def on_open(self, ws):
+        print("WebSocket connection opened")
+        ws.send(json.dumps(self.subscribe_message))
+
+    def run(self):
+        ws = websocket.WebSocketApp(self.websocket_url, on_message=self.on_message)
+        ws.on_open = self.on_open
+        ws.run_forever()
---
+++ start_stream.py
@@ -0,0 +1,26 @@
+from websocket_producer import WebSocketProducer
+from kafka_producer import KafkaProducer
+
+if __name__ == "__main__":
+    # Define configurations
+    websocket_url = "wss://ws-feed.pro.coinbase.com"
+    subscribe_message = {
+        "type": "subscribe",
+        "product_ids": ["ETH-USD", "ETH-EUR"],
+        "channels": [
+            "level2",
+            "heartbeat",
+            {
+                "name": "ticker",
+                "product_ids": ["ETH-BTC", "ETH-USD"]
+            }
+        ]
+    }
+    kafka_bootstrap_servers = 'kafka_bootstrap_servers'  # Replace with your Kafka bootstrap servers
+
+    # Initialize Kafka producer
+    kafka_producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
+
+    # Initialize and run WebSocket producer
+    websocket_producer = WebSocketProducer(websocket_url, subscribe_message, kafka_producer)
+    websocket_producer.run()
